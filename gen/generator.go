a4ae96c4b6659b5f35386c3c924be0c52b68ee1f
Geert-Johan Riemer <geertjohan.riemer@gmail.com>
2013-11-26 13:19:06 +0100

    Added test

--- gen/generator.go
+++ gen/generator.go
@@ -72,8 +72,48 @@ func main() {
 		}
 	}
 
+	// create test.go
+	file, err := os.Create("int_test.go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	file.WriteString(`package incremental
+
+import (
+	"testing"
+)
+
+type user struct {
+	i Int
+}
+
+func TestIntPtr(t *testing.T) {
+	i := &Int{}
+	num := i.Next()
+	if num != 1 {
+		t.Fatal("expected 1, got %d", num)
+	}
+	num = i.Next()
+	if num != 2 {
+		t.Fatal("expected 2, got %d", num)
+	}
+}
+
+func TestAsField(t *testing.T) {
+	u := user{}
+	num := u.i.Next()
+	if num != 1 {
+		t.Fatal("expected 1, got %d", num)
+	}
+	num = u.i.Next()
+	if num != 2 {
+		t.Fatal("expected 2, got %d", num)
+	}
+}`)
+
 	// create doc.go
-	file, err := os.Create("doc.go")
+	file, err = os.Create("doc.go")
 	if err != nil {
 		log.Fatal(err)
 	}
