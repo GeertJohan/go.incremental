88acc9dede4a1fd2ed5fe2e2fc4e2faf7a0fee72
Geert-Johan Riemer <geertjohan@geertjohan.net>
2019-03-16 11:23:48 +0100

    Fix test calls to t.Fatalf
    fixes #2

--- gen/generator.go
+++ gen/generator.go
@@ -65,25 +65,25 @@ func Test{{.Upper}}Ptr(t *testing.T) {
 	i := &{{.Upper}}{}
 	num := i.Next()
 	if num != 1 {
-		t.Fatal("expected 1, got %d", num)
+		t.Fatalf("expected 1, got %d", num)
 	}
 	num = i.Next()
 	if num != 2 {
-		t.Fatal("expected 2, got %d", num)
+		t.Fatalf("expected 2, got %d", num)
 	}
 	num = i.Last()
 	if num != 2 {
-		t.Fatal("expected last to be 2, got %d", num)
+		t.Fatalf("expected last to be 2, got %d", num)
 	}
 
 	i.Set(42)
 	num = i.Last()
 	if num != 42 {
-		t.Fatal("expected last to be 42, got %d", num)
+		t.Fatalf("expected last to be 42, got %d", num)
 	}
 	num = i.Next()
 	if num != 43 {
-		t.Fatal("expected 43, got %d", num)
+		t.Fatalf("expected 43, got %d", num)
 	}
 }
 
@@ -91,39 +91,39 @@ func Test{{.Upper}}AsField(t *testing.T) {
 	s := some{{.Upper}}Struct{}
 	num := s.i.Next()
 	if num != 1 {
-		t.Fatal("expected 1, got %d", num)
+		t.Fatalf("expected 1, got %d", num)
 	}
 	num = s.i.Next()
 	if num != 2 {
-		t.Fatal("expected 2, got %d", num)
+		t.Fatalf("expected 2, got %d", num)
 	}
 	num = s.i.Last()
 	if num != 2 {
-		t.Fatal("expected last to be 2, got %d", num)
+		t.Fatalf("expected last to be 2, got %d", num)
 	}
 
 	useSome{{.Upper}}Struct(&s, t)
 
 	num = s.i.Last()
 	if num != 3 {
-		t.Fatal("expected last to be 3, got %d", num)
+		t.Fatalf("expected last to be 3, got %d", num)
 	}
 
 	s.i.Set(42)
 	num = s.i.Last()
 	if num != 42 {
-		t.Fatal("expected last to be 42, got %d", num)
+		t.Fatalf("expected last to be 42, got %d", num)
 	}
 	num = s.i.Next()
 	if num != 43 {
-		t.Fatal("expected 43, got %d", num)
+		t.Fatalf("expected 43, got %d", num)
 	}
 }
 
 func useSome{{.Upper}}Struct(s *some{{.Upper}}Struct, t *testing.T) {
 	num := s.i.Next()
 	if num != 3 {
-		t.Fatal("expected 3, got %d", num)
+		t.Fatalf("expected 3, got %d", num)
 	}
 }
 `)
--- int_test.go
+++ int_test.go
@@ -12,25 +12,25 @@ func TestIntPtr(t *testing.T) {
 	i := &Int{}
 	num := i.Next()
 	if num != 1 {
-		t.Fatal("expected 1, got %d", num)
+		t.Fatalf("expected 1, got %d", num)
 	}
 	num = i.Next()
 	if num != 2 {
-		t.Fatal("expected 2, got %d", num)
+		t.Fatalf("expected 2, got %d", num)
 	}
 	num = i.Last()
 	if num != 2 {
-		t.Fatal("expected last to be 2, got %d", num)
+		t.Fatalf("expected last to be 2, got %d", num)
 	}
 
 	i.Set(42)
 	num = i.Last()
 	if num != 42 {
-		t.Fatal("expected last to be 42, got %d", num)
+		t.Fatalf("expected last to be 42, got %d", num)
 	}
 	num = i.Next()
 	if num != 43 {
-		t.Fatal("expected 43, got %d", num)
+		t.Fatalf("expected 43, got %d", num)
 	}
 }
 
@@ -38,38 +38,38 @@ func TestIntAsField(t *testing.T) {
 	s := someIntStruct{}
 	num := s.i.Next()
 	if num != 1 {
-		t.Fatal("expected 1, got %d", num)
+		t.Fatalf("expected 1, got %d", num)
 	}
 	num = s.i.Next()
 	if num != 2 {
-		t.Fatal("expected 2, got %d", num)
+		t.Fatalf("expected 2, got %d", num)
 	}
 	num = s.i.Last()
 	if num != 2 {
-		t.Fatal("expected last to be 2, got %d", num)
+		t.Fatalf("expected last to be 2, got %d", num)
 	}
 
 	useSomeIntStruct(&s, t)
 
 	num = s.i.Last()
 	if num != 3 {
-		t.Fatal("expected last to be 3, got %d", num)
+		t.Fatalf("expected last to be 3, got %d", num)
 	}
 
 	s.i.Set(42)
 	num = s.i.Last()
 	if num != 42 {
-		t.Fatal("expected last to be 42, got %d", num)
+		t.Fatalf("expected last to be 42, got %d", num)
 	}
 	num = s.i.Next()
 	if num != 43 {
-		t.Fatal("expected 43, got %d", num)
+		t.Fatalf("expected 43, got %d", num)
 	}
 }
 
 func useSomeIntStruct(s *someIntStruct, t *testing.T) {
 	num := s.i.Next()
 	if num != 3 {
-		t.Fatal("expected 3, got %d", num)
+		t.Fatalf("expected 3, got %d", num)
 	}
 }
